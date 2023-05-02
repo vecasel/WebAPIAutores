2f580236b8a14b79cf1f332e948c5d6306106221
vecasel <danife1986@hotmail.com>
2023-05-02 17:31:02 -0500

    Prueba de push

--- WebAPIAutores/Entidades/Autor.cs
+++ WebAPIAutores/Entidades/Autor.cs
@@ -9,7 +9,7 @@ namespace WebAPIAutores.Entidades
         public int Id { get; set; }
         [Required(ErrorMessage = "El campo {0} es requerido")]
         [StringLength(maximumLength: 120, ErrorMessage = "El campo {0} no debe tener más de {1} caracteres.")]
-        //[PrimeraLetraMayuscula]
+        [PrimeraLetraMayuscula]
         public string Nombre { get; set; }
         //[Range(18, 120)]
         //[NotMapped]
