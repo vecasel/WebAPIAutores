f8936940e86c55b567d65ee077684a610fa561d2
daniel.velasquez <daniel.velasquez@CBAUTISTA1-S3I>
2023-05-02 17:17:22 -0500

    Agregar archivos de proyecto.

---
+++ WebAPIAutores/Controllers/AutoresController.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebAPIAutores.Entidades;
+
+namespace WebAPIAutores.Controllers
+{
+    [ApiController]
+    [Route("api/autores")]
+    public class AutoresController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+        public AutoresController(ApplicationDbContext context)
+        { 
+            this.context = context;
+        }
+        [HttpGet]
+        [HttpGet("listado")]
+        [HttpGet("/listado")]
+        public async Task<ActionResult<List<Autor>>> Get()
+        {
+            return await context.Autores.Include(x => x.Libros).ToListAsync();
+        }
+        [HttpGet("primero")]
+        public async Task<ActionResult<Autor>> PrimerAutor([FromHeader] int miValor, [FromQuery] string nombre)
+        {
+            return await context.Autores.FirstOrDefaultAsync();
+        }
+        [HttpGet("{id:int}/{param2?}")]
+        public async Task<ActionResult<Autor>> Get(int id, string param2)
+        {
+            var autor = await context.Autores.FirstOrDefaultAsync(x => x.Id == id);
+            if (autor is null)
+            {
+                return NotFound();
+            }
+            return autor;
+        }
+        [HttpGet("{nombre}")]
+        public async Task<ActionResult<Autor>> Get([FromRoute] string nombre)
+        {
+            var autor = await context.Autores.FirstOrDefaultAsync(x => x.Nombre.Contains(nombre));
+            if (autor is null)
+            {
+                return NotFound();
+            }
+            return autor;
+        }
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] Autor autor)
+        {
+
+            var existeAutorConElMismoNombre = await context.Autores.AnyAsync(x => x.Nombre == autor.Nombre);
+
+            if (existeAutorConElMismoNombre)
+            {
+                return BadRequest($"Ya existe un autor con el nombre {autor.Nombre}");
+            }
+
+            context.Add(autor);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult> Put(Autor autor, int id)
+        {
+            var existe = await context.Autores.AnyAsync(x => x.Id == id);
+            if (!existe)
+            {
+                return NotFound();
+            }
+            if (autor.Id != id)
+            {
+                return BadRequest("El id del autor no coincide con el id de la URL");
+            }
+            context.Update(autor);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var existe = await context.Autores.AnyAsync(x => x.Id == id);
+            if (!existe)
+            {
+                return NotFound();
+            }
+            context.Remove(new Autor() { Id = id });
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+    }
+}
---
+++ WebAPIAutores/Entidades/Libro.cs
@@ -0,0 +1,13 @@
+﻿using WebAPIAutores.Validaciones;
+
+namespace WebAPIAutores.Entidades
+{
+    public class Libro
+    {
+        public int Id { get; set; }
+        [PrimeraLetraMayuscula]
+        public string Titulo { get; set; }
+        public int AutorId { get; set; }
+        public Autor Autor { get; set; }
+    }
+}
